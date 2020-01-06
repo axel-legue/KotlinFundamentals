d1cac7570b6fd84e8568b04a38d5adc9d9049874
axel.legue <axel.legue31@gmail.com>
2020-01-06 23:05:37 +0100

    Play with Option Menu

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.android.diceroller
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
@@ -16,8 +19,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        setContentView(R.layout.activity_main)
         rollButton = findViewById(R.id.roll_button)
         countUpButton = findViewById(R.id.count_button)
         resetButton = findViewById(R.id.reset_button)
@@ -25,6 +28,25 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener { rollDice() }
         countUpButton.setOnClickListener { countUp() }
         resetButton.setOnClickListener { resetDice() }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.action_add -> {
+                Toast.makeText(this, "Ajouter", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_delete -> {
+                Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun rollDice() {
