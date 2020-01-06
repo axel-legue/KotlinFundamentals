288845c359428649ee4ea07476ff3a42b748a177
axel.legue <axel.legue31@gmail.com>
2020-01-06 23:31:44 +0100

    Play with Style and theme

---
+++ app/src/main/java/com/example/android/diceroller/ThemeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThemeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_theme)
+    }
+}
